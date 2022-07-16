2353ee793ea307a6d97fd37a645f59d2afa600d6
joyderek <joy_derek2022@163.com>
2022-07-16 17:55:50 +0800

    修改gpu

--- examples/run_crf_ner.py
+++ examples/run_crf_ner.py
@@ -597,7 +597,7 @@ def main():
         device = torch.device("cuda", args.local_rank)
         torch.distributed.init_process_group(backend="nccl")
         args.n_gpu = 1
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda")
     args.device = device
 
     # Setup logging
