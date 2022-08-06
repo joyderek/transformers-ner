a9e773f812e28dc609e136b72125ef59970a8ee8
joyderek <joy_derek2022@163.com>
2022-08-06 11:12:56 +0800

    修改batch为16

--- scripts/run_crf_ner.sh
+++ scripts/run_crf_ner.sh
@@ -13,14 +13,16 @@ CUDA_VISIBLE_DEVICES='0,1' python ../examples/run_crf_ner.py \
 --output_dir $OUTPUT_DIR \
 --labels $LABEL \
 --overwrite_output_dir \
+--do_train \
+--do_eval \
 --do_predict \
 --evaluate_during_training \
 --adv_training fgm \
 --num_train_epochs 3 \
 --max_seq_length 512 \
 --logging_steps 0.2 \
---per_gpu_train_batch_size 8 \
---per_gpu_eval_batch_size 8 \
+--per_gpu_train_batch_size 16 \
+--per_gpu_eval_batch_size 16 \
 --learning_rate 5e-5 \
 --bert_lr 5e-5 \
 --classifier_lr  5e-5 \
